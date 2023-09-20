90cc2b201ac9e96c1a093785d8e36e12bc4e64ef
Igor Zakharov <zakharov_igor@inbox.ru>
2023-09-20 18:10:49 +0300

    Add media player func play, next, previous

--- app/src/main/java/com/example/hw1_media_player/MainActivity.kt
+++ app/src/main/java/com/example/hw1_media_player/MainActivity.kt
@@ -1,11 +1,65 @@
 package com.example.hw1_media_player
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.ContentResolver
+import android.media.MediaPlayer
+import android.net.Uri
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.hw1_media_player.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        var numberSongPlayed = 0
+
+        var mediaPlayer = MediaPlayer.create(this, R.raw.bitter_pill)
+        var playList = makePlaylist()
+
+        binding.imageButtonPlayPause.setOnClickListener {
+
+            mediaPlayer.start()
+
+        }
+
+        binding.imageButtonNext.setOnClickListener {
+
+            numberSongPlayed = (numberSongPlayed + 1) % 3
+            mediaPlayer.reset()
+            mediaPlayer.setDataSource(this, playList[numberSongPlayed])
+            mediaPlayer.prepare()
+            mediaPlayer.start()
+        }
+
+        binding.imageButtonPrevious.setOnClickListener {
+
+            numberSongPlayed = if (numberSongPlayed == 0) playList.size - 1 else numberSongPlayed - 1
+            mediaPlayer.reset()
+            mediaPlayer.setDataSource(this, playList[numberSongPlayed])
+            mediaPlayer.prepare()
+            mediaPlayer.start()
+        }
+
     }
+
+
+    fun makePlaylist(): List<Uri> {
+        return mutableListOf<Uri>(
+            uriFromRaw(R.raw.bitter_pill),
+            uriFromRaw(R.raw.save_me_now),
+            uriFromRaw(R.raw.cant_break_me_down)
+        )
+    }
+    fun uriFromRaw(resource: Int): Uri {
+        return Uri.Builder()
+            .scheme(ContentResolver.SCHEME_ANDROID_RESOURCE)
+            .authority(packageName)
+            .path(resource.toString())
+            .build()
+    }
+
 }
